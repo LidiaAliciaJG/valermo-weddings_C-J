d7f1db139be073dc6aed6b206ca7b38511eb8f28
Lidia Alicia JG <lidiaaliciajg@gmail.com>
2025-05-07 01:36:32 -0600

    Rutas relativas changed

--- src/js/router.js
+++
@@ -1,7 +0,0 @@
-const base = document.createElement('base');
-if (location.hostname.includes('github.io')) {
-    base.href = '/valermo-weddings_C-J/';
-} else {
-    base.href = '/';
-}
-document.head.appendChild(base);
